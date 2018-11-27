9363973fe3a3af6d873c1bfa6a47b4b78aadc9d0
Choi Seungeon <chou610@naver.com>
2018-11-27 17:06:08 +0900

    first commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+const path = require('path');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+module.exports = router;
